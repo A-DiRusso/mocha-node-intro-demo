79c2a4c677660ac1903e36c87535d77a3156afe9
Anthony DiRusso <ossurid@gmail.com>
2019-08-09 10:50:33 -0400

    adds seed.sql for coffee db

---
+++ seed.sql
@@ -0,0 +1,8 @@
+insert into coffee
+  (name, bespoke, size)
+VALUES
+  ('Amy', 'Coffee Black', 'Grande'),
+  ('Andy', 'Americano Extra Cream Extra Sugar', 'Venti'),
+  ('Aretha', 'Cappuccino Light Foam', 'Tall'),
+  ('Alex', 'Breve Latte Extra Sugar', 'Extra Grande')
+;
