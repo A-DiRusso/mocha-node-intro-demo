facac4364ae69a08bd78e2d1f7b7b30e85b07466
Anthony DiRusso <ossurid@gmail.com>
2019-08-09 11:23:45 -0400

    adds tdd for coffee db

---
+++ test/test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromsied = require('chai-as-promised');
+chai.use(chaiAsPromsied).should();
+
+const Coffee = require('../models/coffee');
+
+describe('Sanity Check', () => {
+  it('Should be 2', () => {
+    assert.equal(2, 1+1);
+    expect(1 + 1).to.equal(2);
+  });
+});
+
+describe('Coffee Model', async () => {
+  const coffeeData = await Coffee.getById(3);
+  console.log(coffeeData);
+  expect(coffeeData).to.be.an.instanceOf(Coffee);
+});
